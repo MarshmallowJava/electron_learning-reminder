2a170e599ab61d1d7540a9dfe830d2d1510b0654
MarshmallowJava <pironyao17@outlook.jp>
2024-10-11 16:11:14 +0900

    リストを追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 import * as React from 'react';
 import "./App.scss"
+
 import Container from 'react-bootstrap/Container'
-import Button from "react-bootstrap/Button"
+import Table from "react-bootstrap/Table"
 
 export default function App(){
+    const subtitle = ["提出忘れは防がねばならぬ", "ペーパーレスは実現された！", "* 課題は登録しないと効果がないぞ"]
+
     return (
-        <Container fluid>
-            <h1>Reminder</h1>
-            This manages the risk of deadlines.
-            <div className="mt-4">
-                <Button
-                    onClick={() => {
-                        window.subPageAPI.open()
-                    }}
-                >
-                    Open Subwindow
-                </Button>
+        <Container>
+            <div className="title">
+                <h1>REMINDER</h1>
+                {subtitle[Math.floor(Math.random() * subtitle.length)]}
+            </div>
+            <div className="list">
+                <h5>課題一覧</h5>
+                <Table bordered>
+                    <thead>
+                        <td>授業</td>
+                        <td>課題</td>
+                        <td>提出可能期間</td>
+                        <td>提出状況</td>
+                    </thead>
+                </Table>
             </div>
         </Container>
     )
